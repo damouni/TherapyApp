5db6ad0eff50b049722614a360c246d1105a69d1
shahin abu shahin <>
2021-03-14 07:44:20 +0100

    start dev

--- app/src/main/java/com/example/myproject/LoginActivity.java
+++ app/src/main/java/com/example/myproject/LoginActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class SignInActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 EditText email1;
 EditText passw;
     DatabaseHelper databaseHelper;
