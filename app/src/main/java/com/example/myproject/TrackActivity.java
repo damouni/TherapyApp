b4b7d53ba5b97540329bbe70b878f9309409724a
DESKTOP-32P84U6\mhmds <mhmd.shahen19@gmail.com>
2021-03-16 22:46:26 +0200

    Working on TrackActivity Design

--- app/src/main/java/com/example/myproject/TrackActivity.java
+++ app/src/main/java/com/example/myproject/TrackActivity.java
@@ -3,6 +3,7 @@ package com.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class TrackActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class TrackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track);
+    }
+
+    public void AddTrackToDwa(View view)  {
+
     }
 }
