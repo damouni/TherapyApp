fd807dc3dfd66a3bea32aa6fc4558b37c2a1b408
DESKTOP-32P84U6\mhmds <mhmd.shahen19@gmail.com>
2021-03-17 21:32:13 +0200

    notification test for shahen

--- app/src/main/java/com/example/myproject/ChooseDwa.java
+++ app/src/main/java/com/example/myproject/ChooseDwa.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -20,14 +23,19 @@ public class ChooseDwa extends AppCompatActivity {
         BtnNotifacation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String message = "this is a notifacation example";
+                String message = "لقد حان وقت اخذ الدواء اضغط هنا لتأكيد تلقيك للدواء ";
                 NotificationCompat.Builder builder=new NotificationCompat.Builder(ChooseDwa.this)
                         .setSmallIcon(R.drawable.healthcare)
-                        .setContentTitle("new notification")
+                        .setContentTitle("لا تنسى انه وقت اخذ الدواء")
                         .setContentText(message)
                         .setAutoCancel(true);
                 Intent intent =new Intent(ChooseDwa.this, Notification.class);
-
+intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+intent.putExtra("message",message);
+                PendingIntent pendingIntent=  PendingIntent.getActivity(ChooseDwa.this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(pendingIntent);
+                NotificationManager notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.notify(0,builder.build());
             }
         });
     }
